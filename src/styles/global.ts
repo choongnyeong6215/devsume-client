7096b46a1784b10627b93f1311dbb420ce82b9f3
choongnyeong6215 <choong6215@gmail.com>
2025-04-05 02:06:21 +0900

    [Style] 전역 기본 스타일 적용

---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { css } from "@emotion/react";
+
+const GlobalStyle = css`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
+`;
+
+export default GlobalStyle;
