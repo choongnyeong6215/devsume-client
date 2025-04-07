21230deef0be2b4c9fe3474b8275e8a486de53bc
choongnyeong6215 <choong6215@gmail.com>
2025-04-07 23:45:07 +0900

    [Style] Basic Theme 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { Theme } from "@emotion/react";
+
+const lightTheme: Theme = {
+  color: {
+    primary: "#FF9D23",
+    secondary: "#EEEEEE",
+    text: "#000000",
+  },
+  backgroundColor: "#fff",
+  borderRadius: {
+    default: "0.5rem",
+  },
+};
+
+export default lightTheme;
