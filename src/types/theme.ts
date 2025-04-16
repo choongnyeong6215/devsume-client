f3ce9d63d86dae9455b216e1096a0c4403cd5e6d
choongnyeong6215 <choong6215@gmail.com>
2025-04-17 04:05:20 +0900

    [Style] 테마 타입 추가

--- src/types/theme.ts
+++ src/types/theme.ts
@@ -1,1 +1,6 @@
-export type ColorKey = "primary" | "secondary" | "text";
+export type ThemeName = "light" | "dark";
+export type ColorSchema = "background" | "section" | "border" | "text";
+export type ButtonSchema = "filled" | "outlined";
+export type TooltipSchmea = "essential" | "optional";
+export type FontSize = "small" | "medium" | "large";
+export type Radius = "small" | "medium" | "large";
