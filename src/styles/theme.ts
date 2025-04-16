1ec1d6b7f73d68bbfba1e6ec69e7d3dbfedda646
choongnyeong6215 <choong6215@gmail.com>
2025-04-17 04:06:37 +0900

    [Style] Basic Theme 재정의

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,15 +1,47 @@
 import { Theme } from "@emotion/react";
 
-const lightTheme: Theme = {
+export const lightTheme: Theme = {
+  name: "light",
   color: {
-    primary: "#FF9D23",
-    secondary: "#EEEEEE",
+    background: "#F2F4F6",
+    section: "#ffffff",
+    border: "#CED4DA",
     text: "#000000",
   },
-  backgroundColor: "#fff",
-  borderRadius: {
-    default: "0.5rem",
+  button: {
+    filled: {
+      color: "#ffffff",
+      background: "#3182F6",
+      border: "none",
+      padding: "0 1.5rem",
+    },
+    outlined: {
+      color: "#000000",
+      background: "#FFFFFF",
+      border: "1px solid #F8F9FA",
+      padding: "0 1.5rem",
+    },
+  },
+  tooltip: {
+    essential: {
+      color: "#FA5252",
+      background: "#FFF5F5",
+      padding: "0 8px",
+    },
+    optional: {
+      color: "#868E96",
+      background: "#F8F9FA",
+      padding: "0 8px",
+    },
+  },
+  fontSize: {
+    small: "0.75rem",
+    medium: "1rem",
+    large: "1.25rem",
+  },
+  radius: {
+    small: "0.25rem",
+    medium: "0.5rem",
+    large: "1rem",
   },
 };
-
-export default lightTheme;
