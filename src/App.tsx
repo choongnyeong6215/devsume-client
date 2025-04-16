5193238aa38f3ca618d980d3653dd2dc7cb2cb72
choongnyeong6215 <choong6215@gmail.com>
2025-04-17 04:07:13 +0900

    [Style] 전역 스타일, 테마 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import GlobalStyle from "@/styles/global";
+import { lightTheme } from "@/styles/theme";
+import { Global, ThemeProvider } from "@emotion/react";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Global styles={GlobalStyle} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
