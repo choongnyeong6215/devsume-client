f384c000782599dfb4ac626ac7926114b19004fe
choongnyeong6215 <choong6215@gmail.com>
2025-04-17 04:05:42 +0900

    [Style] 테마 타입 재정의

--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -1,12 +1,33 @@
 import "@emotion/react";
-import { ColorKey } from "@types/theme";
+import {
+  ButtonSchema,
+  ColorSchema,
+  FontSize,
+  Radius,
+  ThemeName,
+  TooltipSchmea,
+} from "@/types/theme";
 
 declare module "@emotion/react" {
   export interface Theme {
-    color: Record<ColorKey, string>;
-    backgroundColor: string;
-    borderRadius: {
-      default: string;
+    name: ThemeName;
+    color: Record<ColorSchema, string>;
+    button: {
+      [key in ButtonSchema]: {
+        color: string;
+        background: string;
+        border?: string;
+        padding: string;
+      };
     };
+    tooltip: {
+      [key in TooltipSchmea]: {
+        color: string;
+        background: string;
+        padding: string;
+      };
+    };
+    fontSize: Record<FontSize, string>;
+    radius: Record<Radius, string>;
   }
 }
