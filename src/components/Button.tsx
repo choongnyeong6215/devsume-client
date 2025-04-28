07d5d7b58c0baf05863a07d794fe7f38a9aea7aa
choongnyeong6215 <choong6215@gmail.com>
2025-04-28 23:56:12 +0900

    [Feat] 버튼 공통 컴포넌트 구현

---
+++ src/components/Button.tsx
@@ -0,0 +1,39 @@
+import { ButtonSchema, Radius } from "@/types/theme";
+import styled from "@emotion/styled";
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import { darken } from "polished";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  schema: ButtonSchema;
+  radiusSize: Radius;
+  children: ReactNode;
+}
+
+const Button = ({ schema, radiusSize, children, ...rest }: ButtonProps) => {
+  return (
+    <StyledButton schema={schema} radiusSize={radiusSize} {...rest}>
+      {children}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button<{
+  schema: ButtonSchema;
+  radiusSize: Radius;
+}>`
+  ${({ theme, schema, radiusSize }) => ({
+    color: theme.button[schema].color,
+    backgroundColor: theme.button[schema].background,
+    padding: theme.button[schema].padding,
+    border: theme.button[schema].border,
+    borderRadius: theme.borderRadius[radiusSize],
+    height: "2rem",
+    cursor: "pointer",
+
+    "&:hover": {
+      backgroundColor: darken(0.05, theme.button[schema].background),
+    },
+  })}
+`;
+
+export default Button;
