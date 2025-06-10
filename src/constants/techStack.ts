7563368428ba4c420df10e8e7f398b7b25c06489
choongnyeong6215 <choong6215@gmail.com>
2025-06-10 22:25:02 +0900

    [Add] 기술 스택 옵션 상수 추가

---
+++ src/constants/techStack.ts
@@ -0,0 +1,45 @@
+const LANGUAGE = ["JAVASCRIPT", "TYPESCRIPT", "JAVA", "C#", "C++", "Python"];
+
+const FRONTEND = [
+  "HTML/CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "React Native",
+  "Next.js",
+  "Recoil",
+  "Redux",
+  "Vue.js",
+  "Zustand",
+  "scss",
+  "TailwindCSS",
+  "styled-components",
+  "emotion",
+  "Tanstack Query",
+];
+
+const BACKEND = [
+  "Express",
+  "NestJS",
+  "Spring",
+  "Spring Boot",
+  "Spring Security",
+];
+
+const DATABASE = ["MySQL", "MongoDB", "PostgreSQL", "Redis"];
+
+const TESTTOOL = ["Jest", "React Testing Library"];
+
+const INFRA = ["Docker", "Kubernetes"];
+
+const EXTRA = ["Github"];
+
+export const TECHSTACK = [
+  ...LANGUAGE,
+  ...FRONTEND,
+  ...BACKEND,
+  ...DATABASE,
+  ...TESTTOOL,
+  ...INFRA,
+  ...EXTRA,
+];
