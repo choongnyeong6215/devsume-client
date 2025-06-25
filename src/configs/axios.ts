ae8a6a2f7f750a67d532a5d2701dfaf7681c2883
choongnyeong6215 <choong6215@gmail.com>
2025-06-26 03:23:33 +0900

    [Modify] 상수 -> 환경변수 적용

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,9 +1,8 @@
-import { BASE_URL, DEFAULT_TIMEOUT } from "@/constants/axios";
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  timeout: DEFAULT_TIMEOUT,
+  baseURL: import.meta.env.BASE_URL,
+  timeout: import.meta.env.DEFAULT_TIMEOUT,
   headers: {
     "Content-Type": "application/json",
   },
