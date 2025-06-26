e2d958db51634b7bad0b4cc0e9c80b4601dedf69
choongnyeong6215 <choong6215@gmail.com>
2025-06-26 17:57:28 +0900

    [Feat] Interceptor 추가

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,3 +1,4 @@
+import { useAuthStore } from "@/store/auth";
 import axios from "axios";
 
 export const axiosInstance = axios.create({
@@ -8,3 +9,41 @@ export const axiosInstance = axios.create({
   },
   withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  const accessToken = useAuthStore.getState().accessToken;
+
+  if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
+
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (err) => {
+    const originalRequest = err.config;
+
+    if (err.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const response = await axiosInstance.post("/users/refresh");
+        const newAccessToken = response.data.accessToken;
+
+        // 스토어 토큰 변경
+        useAuthStore.getState().setAccessToken(newAccessToken);
+
+        // 헤더 변경
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        // 재요청
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        useAuthStore.getState().clearAccessToken();
+
+        return Promise.reject(err);
+      }
+    }
+    return Promise.reject(err);
+  }
+);
