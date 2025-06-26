97ad5c1994b4695fca0131c08237adc406b24b01
choongnyeong6215 <choong6215@gmail.com>
2025-06-27 08:36:44 +0900

    [Feat] 로그인 api 호출 서비스 추가

---
+++ src/features/auth/services/login.ts
@@ -0,0 +1,11 @@
+import { axiosInstance } from "@/configs/axios";
+import { UserInfo } from "@/features/auth/types/index.ts";
+import { useAuthStore } from "@/store/auth";
+
+export const login = async (credentials: UserInfo) => {
+  const response = await axiosInstance.post("/users/local/login", credentials);
+
+  useAuthStore.getState().setAccessToken(response.data.accessToken);
+
+  return response.data;
+};
