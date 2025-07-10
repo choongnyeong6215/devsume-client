3c6965f341b659341e75b5fb4f1a5b917aececf3
choongnyeong6215 <choong6215@gmail.com>
2025-07-10 22:07:20 +0900

    [Modify] rhf 에러 메시지 연동

--- src/features/resume/components/formField/ErrorMessage.tsx
+++ src/features/resume/components/formField/ErrorMessage.tsx
@@ -1,16 +1,21 @@
 import styled from "@emotion/styled";
 import { useFormField } from "@/hooks/useFormField";
+import { useFormContext } from "react-hook-form";
 
 const ErrorMessage = () => {
-  const { errorMessage } = useFormField();
+  const { id } = useFormField();
+  const {
+    formState: { errors },
+  } = useFormContext();
 
-  return errorMessage ? (
-    <StyledErrorMessage>{errorMessage} </StyledErrorMessage>
+  return errors[id] ? (
+    <StyledErrorMessage>{errors[id]?.message?.toString()}</StyledErrorMessage>
   ) : null;
 };
 
 const StyledErrorMessage = styled.p`
   color: red;
+  font-size: 0.75rem;
 `;
 
 export default ErrorMessage;
