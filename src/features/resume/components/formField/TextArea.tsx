c23ee4bebc0c2684f97886776a9099a2ab200c6d
choongnyeong6215 <choong6215@gmail.com>
2025-07-10 22:06:32 +0900

    [Modify] 최대 입력 글자 설정

--- src/features/resume/components/formField/TextArea.tsx
+++ src/features/resume/components/formField/TextArea.tsx
@@ -1,11 +1,15 @@
 import { useFormField } from "@/hooks/useFormField";
 import styled from "@emotion/styled";
 import { TextareaHTMLAttributes } from "react";
+import { useFormContext } from "react-hook-form";
 
 const TextArea = (props: TextareaHTMLAttributes<HTMLTextAreaElement>) => {
   const { id } = useFormField();
+  const { register } = useFormContext();
 
-  return <StyledTextArea id={id} {...props} />;
+  return (
+    <StyledTextArea id={id} {...register(id)} {...props} maxLength={300} />
+  );
 };
 
 const StyledTextArea = styled.textarea`
