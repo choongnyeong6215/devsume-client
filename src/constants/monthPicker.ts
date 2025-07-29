b91df85568b841ea6b6df0ab23804d078e754fb0
choongnyeong6215 <choong6215@gmail.com>
2025-07-29 15:58:33 +0900

    [Add] 달력 년 & 월 옵션 상수 추가

---
+++ src/constants/monthPicker.ts
@@ -0,0 +1,16 @@
+export const CURRENT_YEAR = new Date().getFullYear();
+
+export const MONTH = [
+  "1월",
+  "2월",
+  "3월",
+  "4월",
+  "5월",
+  "6월",
+  "7월",
+  "8월",
+  "9월",
+  "10월",
+  "11월",
+  "12월",
+];
