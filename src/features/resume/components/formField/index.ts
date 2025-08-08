2ff750b4bc3a56c6f2bd6cf845b8566980b73e4d
choongnyeong6215 <choong6215@gmail.com>
2025-08-08 20:20:47 +0900

    [Modify] 마크다운 에디터 컴포넌트 변경

--- src/features/resume/components/formField/index.ts
+++ src/features/resume/components/formField/index.ts
@@ -6,10 +6,10 @@ import ErrorMessage from "@/features/resume/components/formField/ErrorMessage";
 import TextArea from "@/features/resume/components/formField/TextArea";
 import ProfileUplodaer from "@/features/resume/components/upload/ProfileUploader";
 import PortfolioUploader from "@/features/resume/components/upload/PortfolioUploader";
-import MarkdownEditor from "@/features/resume/components/formField/MarkdownEditor";
 import SelectInput from "@/features/resume/components/formField/SelectInput";
 import SearchInput from "@/features/resume/components/formField/SearchInput";
 import MonthRangePicker from "@/features/resume/components/formField/MonthRangePicker";
+import MarkDownEditor from "@/features/resume/components/markdown/MarkdownEditor";
 
 export const FormField = Object.assign(Root, {
   Label,
@@ -19,8 +19,8 @@ export const FormField = Object.assign(Root, {
   TextArea,
   ProfileUplodaer,
   PortfolioUploader,
-  MarkdownEditor,
   SelectInput,
   SearchInput,
   MonthRangePicker,
+  MarkDownEditor,
 });
